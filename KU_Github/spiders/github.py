0859271bbe1e38ceeea04aa54bb0390ee2f113b4
MyeongJun Kim <82578738+johnkim6823@users.noreply.github.com>
2024-03-23 10:30:06 +0900

    Update github.py

--- KU_Github/spiders/github.py
+++ KU_Github/spiders/github.py
@@ -58,77 +58,132 @@ class Gitpider(scrapy.Spider):
             repo_item['LicenseName'] = repo['license']['name'] if repo['license'] else None
             repo_item['ProjectDescription'] = repo['description']
 
+
             # Fetch additional data for each repository
             languages_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/languages'
-            yield scrapy.Request(languages_url, headers=response.request.headers, callback=self.parse_languages, meta={'repo_item': repo_item})
-
-            contributors_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/contributors'
-            yield scrapy.Request(contributors_url, headers=response.request.headers, callback=self.parse_contributors, meta={'repo_item': repo_item})
-
-            commits_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/commits'
-            yield scrapy.Request(commits_url, headers=response.request.headers, callback=self.parse_commits, meta={'repo_item': repo_item})
-
-            readme_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/readme'
-            yield scrapy.Request(readme_url, headers=response.request.headers, callback=self.parse_readme, meta={'repo_item': repo_item})
-
-            release_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/releases/latest'
-            yield scrapy.Request(release_url, headers=response.request.headers, callback=self.parse_release, meta={'repo_item': repo_item})
+            yield scrapy.Request(languages_url, headers=response.request.headers, callback=self.parse_languages, meta={'repo_item': repo_item, 'GithubID': GithubID, 'repo_name': repo["name"]})
 
     def parse_languages(self, response):
         repo_item = response.meta['repo_item']
         languages = json.loads(response.text) if response.text else {}
         repo_item['ProgrammingLanguage'] = list(languages.keys()) if languages else None
-        yield repo_item
+        
+        # Fetch contributors
+        contributors_url = f'{API_URL}/repos/{response.meta["GithubID"]}/{response.meta["repo_name"]}/contributors'
+        yield scrapy.Request(contributors_url, headers=response.request.headers, callback=self.parse_contributors, meta={'repo_item': repo_item})
 
     def parse_contributors(self, response):
         repo_item = response.meta['repo_item']
         contributors = json.loads(response.text) if response.text else []
         repo_item['Contributors'] = [contributor['login'] for contributor in contributors if 'login' in contributor] if contributors else None
-        yield repo_item
+        
+        # Fetch commits
+        commits_url = f'{API_URL}/repos/{response.meta["GithubID"]}/{response.meta["repo_name"]}/commits'
+        yield scrapy.Request(commits_url, headers=response.request.headers, callback=self.parse_commits, meta={'repo_item': repo_item})
 
     def parse_commits(self, response):
         repo_item = response.meta['repo_item']
         commits = json.loads(response.text) if response.text else []
-        repo_item['CommitCount'] = len(commits) if commits else 0  # Or set to None if you prefer
-        yield repo_item
+        repo_item['CommitCount'] = len(commits) if commits else 0
+        
+        # Fetch readme
+        readme_url = f'{API_URL}/repos/{response.meta["GithubID"]}/{response.meta["repo_name"]}/readme'
+        yield scrapy.Request(readme_url, headers=response.request.headers, callback=self.parse_readme, meta={'repo_item': repo_item})
 
-    def parse_readme(self, response):
+     def parse_readme(self, response):
         repo_item = response.meta['repo_item']
-        # If the status is 404 or another non-200, set HasReadME to None
-        repo_item['HasReadME'] = True if response.status == 200 else None
-        yield repo_item
+        repo_item['HasReadME'] = True if response.status == 200 else False
+        
+        # Fetch release information
+        release_url = f'{API_URL}/repos/{response.meta["GithubID"]}/{response.meta["repo_name"]}/releases/latest'
+        yield scrapy.Request(release_url, headers=response.request.headers, callback=self.parse_release, meta={'repo_item': repo_item})
 
     def parse_release(self, response):
         repo_item = response.meta['repo_item']
         if response.status == 200 and response.text:
             release = json.loads(response.text)
-            repo_item['ReleaseVersion'] = release['tag_name']
-        elif response.status == 404:
-            repo_item['ReleaseVersion'] =  None
+            repo_item['ReleaseVersion'] = release.get('tag_name', None)
         else:
-            pass
-        yield repo_item
+            repo_item['ReleaseVersion'] = None
         
-    def parse_issues(self, response):
-        issues = json.loads(response.text)
-        for issue in issues:
-            issue_item = ISSUE()
-            issue_item['ISSUEID'] = issue['id']
-            issue_item['IssuePublisherID'] = issue['user']['login']
-            issue_item['OwnerGithubID'] = issue['repository']['owner']['login']
-            issue_item['RepoURL'] = issue['repository_url']
-            issue_item['IssueDate'] = issue['created_at']
-            issue_item['Title'] = issue['title']
-            yield issue_item
+        # Now that all data is collected, yield the repo_item
+        yield repo_item
 
-    def parse_pulls(self, response):
-        pulls = json.loads(response.text)
-        for pr in pulls:
-            pr_item = PR()
-            pr_item['PRID'] = pr['id']
-            pr_item['RequesterID'] = pr['user']['login']
-            pr_item['OwnerGithubID'] = pr['head']['repo']['owner']['login']
-            pr_item['RepoURL'] = pr['url']
-            pr_item['PRDate'] = pr['created_at']
-            pr_item['Title'] = pr['title']
-            yield pr_item
+            # # Fetch additional data for each repository
+            # languages_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/languages'
+            # yield scrapy.Request(languages_url, headers=response.request.headers, callback=self.parse_languages, meta={'repo_item': repo_item})
+
+            # #contributors_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/contributors'
+            # #yield scrapy.Request(contributors_url, headers=response.request.headers, callback=self.parse_contributors, meta={'repo_item': repo_item})
+
+            # commits_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/commits'
+            # yield scrapy.Request(commits_url, headers=response.request.headers, callback=self.parse_commits, meta={'repo_item': repo_item})
+
+            # readme_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/readme'
+            # yield scrapy.Request(readme_url, headers=response.request.headers, callback=self.parse_readme, meta={'repo_item': repo_item})
+
+            # release_url = f'{API_URL}/repos/{GithubID}/{repo["name"]}/releases/latest'
+            # yield scrapy.Request(release_url, headers=response.request.headers, callback=self.parse_release, meta={'repo_item': repo_item})
+
+    # def parse_languages(self, response):
+    #     repo_item = response.meta['repo_item']
+    #     languages = json.loads(response.text) if response.text else {}
+    #     repo_item['ProgrammingLanguage'] = list(languages.keys()) if languages else None
+        
+    #     # Continue the chain by fetching the next piece of data
+    #     contributors_url = f'{API_URL}/repos/{response.meta["GithubID"]}/{response.meta["repo_name"]}/contributors'
+    #     yield scrapy.Request(contributors_url, headers=response.request.headers, callback=self.parse_contributors, meta={'repo_item': repo_item})
+
+
+    # def parse_contributors(self, response):
+    #     repo_item = response.meta['repo_item']
+    #     contributors = json.loads(response.text) if response.text else []
+    #     repo_item['Contributors'] = [contributor['login'] for contributor in contributors if 'login' in contributor] if contributors else None
+    #     yield repo_item
+
+    # def parse_commits(self, response):
+    #     repo_item = response.meta['repo_item']
+    #     commits = json.loads(response.text) if response.text else []
+    #     repo_item['CommitCount'] = len(commits) if commits else 0  # Or set to None if you prefer
+    #     yield repo_item
+
+    # def parse_readme(self, response):
+    #     repo_item = response.meta['repo_item']
+    #     # If the status is 404 or another non-200, set HasReadME to None
+    #     repo_item['HasReadME'] = True if response.status == 200 else None
+    #     yield repo_item
+
+    # def parse_release(self, response):
+    #     repo_item = response.meta['repo_item']
+    #     if response.status == 200 and response.text:
+    #         release = json.loads(response.text)
+    #         repo_item['ReleaseVersion'] = release['tag_name']
+    #     elif response.status == 404:
+    #         repo_item['ReleaseVersion'] =  None
+    #     else:
+    #         pass
+    #     yield repo_item
+        
+    # def parse_issues(self, response):
+    #     issues = json.loads(response.text)
+    #     for issue in issues:
+    #         issue_item = ISSUE()
+    #         issue_item['ISSUEID'] = issue['id']
+    #         issue_item['IssuePublisherID'] = issue['user']['login']
+    #         issue_item['OwnerGithubID'] = issue['repository']['owner']['login']
+    #         issue_item['RepoURL'] = issue['repository_url']
+    #         issue_item['IssueDate'] = issue['created_at']
+    #         issue_item['Title'] = issue['title']
+    #         yield issue_item
+
+    # def parse_pulls(self, response):
+    #     pulls = json.loads(response.text)
+    #     for pr in pulls:
+    #         pr_item = PR()
+    #         pr_item['PRID'] = pr['id']
+    #         pr_item['RequesterID'] = pr['user']['login']
+    #         pr_item['OwnerGithubID'] = pr['head']['repo']['owner']['login']
+    #         pr_item['RepoURL'] = pr['url']
+    #         pr_item['PRDate'] = pr['created_at']
+    #         pr_item['Title'] = pr['title']
+    #         yield pr_item
